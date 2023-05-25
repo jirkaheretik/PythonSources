af37345eb107ed83a4f5c4cbbdd1610f0552d9ff
Jirka Braza <j@braza.cz>
2023-05-25 11:53:46 +0200

    Updated Clovek and Zvire, eq example

--- Clovek.py
+++ Clovek.py
@@ -1,3 +1,12 @@
+"""
+Test for __eq__(), which, contrary to other languages:
+- calls the same function if comparing c1==c2 and c2==c1 if one object
+  is a subclass of other object class (and __eq__ is overridden)
+- therefore much harder to break __eq__ contract, though there is actually
+  none in Python
+
+"""
+
 class Clovek:
     def __init__(self, jmeno, vek):
         self._jmeno = jmeno
@@ -9,6 +18,14 @@ class Clovek:
     def __str__(self):
         return f"{self._jmeno}, {self._vek}"
 
+    def __eq__(self, xxxxx):
+        print("Clovek.Eq called %r == %r" % (self, xxxxx))
+        if not isinstance(xxxxx, Clovek):
+            print("Clovek.Eq - the other object is not Clovek")
+            return False
+        print("Clovek.Eq - comparing values")
+        return self._jmeno == xxxxx._jmeno and self._vek == xxxxx._vek
+
 
 class Programator(Clovek):
     def __init__(self, jmeno, vek, jazyk):
@@ -19,8 +36,25 @@ class Programator(Clovek):
             print("Hello world!")
         else:
             super(Programator, self).pozdrav(pozdrav)
+    def __eq__(self, other):
+        print("Pgmer.Eq called %r == %r" % (self, other))
+        if not isinstance(other, Programator):
+            print("Pgmer.Eq - the other object is not Pgmer")
+            return False
+        print("Pgmer.Eq - comparing values")
+        return self._jmeno == other._jmeno and self._vek == other._vek and self._jazyk == other._jazyk
 
-karel = Programator("Karel", 42, "Python")
-karel.pozdrav()
-karel.pozdrav("Čau")
-print(karel)
+c1 = Clovek("Karel", 42)
+c2 = Programator("Karel", 42, "Python")
+c3 = c1
+print("Začátek")
+if c2 == c1:
+    print("Stejné")
+else:
+    print("Různé")
+
+if c1 == c2:
+    print("Tohle taky stejné")
+else:
+    print("Tohle taky různé")
+print("Konec.")
--- Zvire.py
+++ Zvire.py
@@ -11,7 +11,38 @@ class Zvire:
         if jidlo > 0:
             self._vaha += jidlo
     def vypis(self):
-        print(f"Jsem {self._popis}, vážím {self._vaha} kg a " + ("létám" if self.leta else "nelétám"))
+        print(self)
+
+    def mluv(self):
+        return ""
+
+    def __str__(self):
+        return f"Jsem {self._popis}, vážím {self._vaha} kg a " + ("létám" if self.leta else "nelétám")
+
+class Pes(Zvire):
+    def __init__(self, popis, vaha, obojek):
+        super().__init__(popis, vaha)
+        self._obojek = obojek
+
+    def mluv(self):
+        return "Haf haf!"
+
+class Kocka(Zvire):
+    def __init__(self, popis, vaha, zivoty):
+        super().__init__(popis, vaha)
+        self._zivoty = zivoty
+
+    def mluv(self):
+        return "Mňaauu :-P"
+
+class Krava(Zvire):
+    def __init__(self, popis, vaha, dojivost):
+        super().__init__(popis, vaha)
+        self._litry = dojivost
+
+    def mluv(self):
+        return "Bůůůůů"
+
 
 
 if __name__ == '__main__':
@@ -19,3 +50,9 @@ if __name__ == '__main__':
     vrana.vypis()
     vrana.nakrm(3)
     vrana.vypis()
+    zoo = [Pes("Alik", 11, True), Kocka("Micka", 6, 7), Krava("Stračena", 222, 11), vrana, Pes("Fousek", 20, False)]
+
+    for zvire in zoo:
+        print(zvire)
+        print(zvire.mluv())
+
