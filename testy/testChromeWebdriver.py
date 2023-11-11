d6d63d850c7e9aa815ee8f8436eb14c9589c189c
Jirka Braza <j@braza.cz>
2023-11-11 17:19:51 +0100

    Updated Chrome Webdriver Selenium test to match change in functionality.

--- testy/testChromeWebdriver.py
+++ testy/testChromeWebdriver.py
@@ -6,11 +6,17 @@ from selenium.webdriver.common.by import By
 class WebdriverTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome()
+        print("Driver created.")
+
     def test_spustit(self):
         self.driver.get("http://www.google.com/")
         time.sleep(5)
-        self.driver.findElement(By.name("q")).sendKeys("ChromeDriver")
-        self.driver.findElement(By.name("q")).submit()
+        print(f"URL {self.driver.current_url}, page title {self.driver.title}")
+        elem = self.driver.find_element(By.NAME, "q") # now the ID is different
+        elem.send_keys("ChromeDriver")
+        elem.submit()
+        time.sleep(3)
+        print(f"URL {self.driver.current_url}, page title {self.driver.title}")
         time.sleep(5)
 
     def tearDown(self) -> None:
